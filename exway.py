7a80b17087e62ffeeea30da5bac26eb98f888004
Ivan Neto <ivan.cr.neto@gmail.com>
2015-02-20 09:40:56 -0300

    server with first view

--- exway.py
+++ exway.py
@@ -1,9 +1,16 @@
 from pyramid.config import Configurator
 from wsgiref.simple_server import make_server
+from pyramid.response import Response
+
+
+def home(request):
+    return Response('Exway!')
 
 
 if __name__ == '__main__':
     config = Configurator()
+    config.add_route('home', '/')
+    config.add_view(home, route_name='home')
     app = config.make_wsgi_app()
 
     server = make_server('0.0.0.0', 6543, app)
